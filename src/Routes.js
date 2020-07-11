7ff0debf24e569dfdab9981969887cf01c41baa4
chiragdoctor <chirag.doctor@gmail.com>
2020-07-11 16:26:31 +0100

    add react-router-dom

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Signin from './user/Signin';
+import Signup from './user/Signup';
+import Home from './core/Home';
+
+const Routes = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/signin' exact component={Signin} />
+        <Route path='/signup' exact component={Signup} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
