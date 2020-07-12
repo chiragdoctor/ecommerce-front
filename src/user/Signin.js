d48d0e0dc00e1d75ce8373bb91fcc0a22329d812
chiragdoctor <chirag.doctor@gmail.com>
2020-07-12 16:35:26 +0100

    Signin Component

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,10 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../core/Layout';
+import { Redirect } from 'react-router-dom';
+import { signin } from '../auth';
+const Signin = () => {
+  const [values, setValues] = useState({
+    email: '',
+    password: '',
+    error: '',
+    loading: false,
+    redirectToReferrer: false,
+  });
 
-const Signin = () => (
-  <Layout title='Signin Page' description='This is my singin page'>
-    <div>Signin page</div>
-  </Layout>
-);
+  const { email, password, error, loading, redirectToReferrer } = values;
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    signin({ email, password }).then((data) => {
+      if (data.error) {
+        setValues({
+          ...values,
+          error: data.error,
+          loading: false,
+        });
+      } else {
+        setValues({
+          ...values,
+          loading: false,
+          redirectToReferrer: true,
+        });
+      }
+    });
+  };
+
+  const showError = () => (
+    <div className='alert alert-danger' style={{ display: error ? '' : 'none' }}>
+      {error}
+    </div>
+  );
+
+  const showLoading = () => (
+    <div className='alert alert-info' style={{ display: loading ? '' : 'none' }}>
+      <h2>Loading ...</h2>
+    </div>
+  );
+
+  const redirectUser = () => {
+    if (redirectToReferrer) {
+      return <Redirect to='/' />;
+    }
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className='form-group'>
+        <label className='text-muted'>Email</label>
+        <input onChange={handleChange('email')} type='email' className='form-control' value={email} />
+      </div>
+      <div className='form-group'>
+        <label className='text-muted'>Password</label>
+        <input onChange={handleChange('password')} type='password' className='form-control' value={password} />
+      </div>
+      <button onClick={clickSubmit} className='btn btn-primary'>
+        Submit
+      </button>
+    </form>
+  );
+  return (
+    <Layout title='Signup Page' description='This is my singup page' className='container col-md-8 offset-md-2'>
+      {showError()}
+      {showLoading()}
+      {signUpForm()}
+      {redirectUser()}
+    </Layout>
+  );
+};
 
 export default Signin;
