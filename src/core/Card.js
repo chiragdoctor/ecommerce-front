403aec4067f3697ede44d62f7524ad216dc24b9e
chiragdoctor <chirag.doctor@gmail.com>
2020-07-23 17:58:31 +0100

    Cart helper add Items to cart

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
+import { addItem } from './cartHelpers';
+
 const Card = ({ product, showViewProductButton = true }) => {
+  const [redirect, setRedirect] = useState(false);
+
   const showViewButton = (showViewProductButton) => {
     return (
       showViewProductButton && (
         <Link to={`/product/${product._id}`}>
-          <button className='btn btn-outline-primary mt-2 mb-2 mr-2'> View Product</button>
+          <button className='btn btn-outline-primary mt-2 mb-2 mr-2'>
+            {' '}
+            View Product
+          </button>
         </Link>
       )
     );
   };
 
+  const addToCart = () => {
+    addItem(product, () => {
+      setRedirect(true);
+    });
+  };
+
+  const shouldRedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to='/cart' />;
+    }
+  };
+
   const showAddToCartButton = () => {
-    return <button className='btn btn-outline-warning mt-2 mb-2'> Add to Cart</button>;
+    return (
+      <button className='btn btn-outline-warning mt-2 mb-2' onClick={addToCart}>
+        {' '}
+        Add to Cart
+      </button>
+    );
   };
 
   const showStock = (quantity) => {
@@ -29,11 +53,16 @@ const Card = ({ product, showViewProductButton = true }) => {
     <div className='card'>
       <div className='card-header name'>{product.name}</div>
       <div className='card-body'>
+        {shouldRedirect(redirect)}
         <ShowImage item={product} url='products' />
         <p className='lead mt-2'>{product.description.substring(0, 100)}</p>
         <p className='black-10'>£{product.price}</p>
-        <p className='black-9'>Category: {product.category && product.category.name}</p>
-        <p className='black-8'>Added on {moment(product.createdAt).fromNow()}</p>
+        <p className='black-9'>
+          Category: {product.category && product.category.name}
+        </p>
+        <p className='black-8'>
+          Added on {moment(product.createdAt).fromNow()}
+        </p>
         {showStock(product.quantity)}
         <br />
         {showViewButton(showViewProductButton)}
