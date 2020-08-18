86cd87eec179fd24063a79ea62a82b1fe18ba548
chiragdoctor <chirag.doctor@gmail.com>
2020-08-18 17:59:36 +0100

    Display User Purchase History

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
+import { getUserPurchaseHistory } from './apiUser';
 
 const UserDashboard = () => {
+  const [history, setHistory] = useState([]);
   const {
     user: { name, email, role, _id },
+    token,
   } = isAuthenticated();
+
+  const init = () => {
+    getUserPurchaseHistory(_id, token).then((data) => {
+      console.log(data);
+      if (data.error) {
+        console.log('Error in fetching user purchase history', data.error);
+      } else {
+        setHistory(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
 
   const userLinks = () => {
     return (
@@ -41,12 +60,29 @@ const UserDashboard = () => {
     );
   };
 
-  const purchaseHistory = () => {
+  const purchaseHistory = (history) => {
     return (
       <div className='card mb-5'>
-        <h3 className='card-header'>Purchase History</h3>
+        <h3 className='card-header'>Purchase history</h3>
         <ul className='list-group'>
-          <li className='list-group-item'>name</li>
+          <li className='list-group-item'>
+            {history.map((h, i) => {
+              return (
+                <div>
+                  <hr />
+                  {h.products.map((p, i) => {
+                    return (
+                      <div key={i}>
+                        <h6>Product name: {p.name}</h6>
+                        <h6>Product price: £{p.price}</h6>
+                        <h6>Purchased date: {moment(p.createdAt).fromNow()}</h6>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </li>
         </ul>
       </div>
     );
@@ -62,7 +98,7 @@ const UserDashboard = () => {
         <div className='col-3'>{userLinks()}</div>
         <div className='col-9'>
           {userInfo()}
-          {purchaseHistory()}
+          {purchaseHistory(history)}
         </div>
       </div>
     </Layout>
